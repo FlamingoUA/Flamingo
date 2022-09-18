5d9ae333e22ca4732c606755835f8661c2e38222
paska <lxpaskar@gmail.com>
2022-09-19 00:27:38 +0200

    VOC-2 add drf; REST simple principles;

--- flamingo_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlamingoApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'flamingo_api'
--- flamingo_api/views.py
+++ flamingo_api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import (
+    CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView,
+)
+from rest_framework import permissions, filters
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from model.models import Ticket
+from .serializers import TicketSerializer
+
+
+class TickerCreateView(CreateAPIView):
+    serializer_class = TicketSerializer
+    queryset = Ticket.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TickerListView(ListAPIView):
+    serializer_class = TicketSerializer
+    queryset = Ticket.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PageNumberPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['price']
+
+
+class TicketRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TicketSerializer
+    lookup_url_kwarg = 'ticket_id'
+    queryset = Ticket.objects.all()
